746a6cc74ac78b24abb14bb0eb836ba3cdfae0c4
muraliram728 <muraliraman728@gmail.com>
2024-08-21 11:49:02 +0530

    Initial commit

---
+++ my-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: "Hello from your custom API!" });
+}); 
+
+// Define a root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js API!');
+});
+
+app.listen(port, () => {
+    console.log(`API running on http://localhost:${port}`);
+});
